c2a457cdf91632010c485e147de811d0e0a2047f
sarapmagcode <85553852+sarapmagcode@users.noreply.github.com>
2023-05-17 13:22:04 +0800

    Update Main.kt

--- Main.kt
+++ Main.kt
@@ -3,6 +3,9 @@ import kotlinx.coroutines.launch
 
 fun main() {
     val states = arrayOf("Starting", "Doing Task 1", "Doing Task 2", "Ending")
+    
+    // For every task or process, use a coroutine.
+    // Let's say you have 3 tasks, then there will be 3 coroutines which will process them concurrently.
     repeat(3) {
         GlobalScope.launch {
             println("${Thread.currentThread()} has started")
